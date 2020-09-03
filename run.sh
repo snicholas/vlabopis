5153e5f774865f85ecd2b2e6e160f6a1586aee99
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 14:10:23 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 echo "Model start"
-pwd
-ls -l
-
 
 if [ -f data/images.tar.gz -a -f data/annotations.tar.gz ]; then
     mkdir -p /tmp/data
     cp data/images.tar.gz /tmp/data/images.tar.gz
     cp data/annotations.tar.gz /tmp/data/annotations.tar.gz
+    ls -lh /tmp/data/
     echo "Unpackaging images.tar.gz"
     tar -xf /tmp/data/images.tar.gz
     echo "Unpackaging annotations.tar.gz"
     tar -xf /tmp/data/annotations.tar.gz
-    ls -l data/    
+    ls -lh /tmp/data/    
     echo "run training"
     python main.py 1
     echo "ended training"
