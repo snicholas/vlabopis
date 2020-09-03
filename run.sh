48aa3be0681edc822cd2a2c1a652b9e2a71ec3ac
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 10:06:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,13 @@ if [ -f /data/images.tar.gz -a -f /data/annotations.tar.gz ]; then
     tar -xf /data/annotations.tar.gz
     echo "run training"
     python main.py 1
+    echo "ended training"
 fi
 
 if [ -f /data/oxford_segmentation.h5 -a -f /data/image.png ]; then
     cp /data/oxford_segmentation.h5 /data/outputs/oxford_segmentation.h5
     echo "run execution"
     python main.py 0
+    echo "ended execution"
 fi
 
