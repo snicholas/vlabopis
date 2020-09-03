c9de36cf19cd681dc316fd024e0a590b506d5755
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 11:39:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ if mode==0:
     if (batch_size>32):
         batch_size=32
     print("batch_size = {0}".format(batch_size))
-    val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, [])
+    val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_input_img_paths)
     val_preds = model.predict(val_gen)
     save_mask("prediction", 0)
 elif mode==1:
