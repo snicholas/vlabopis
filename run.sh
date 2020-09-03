49116fc94847bdb483d2701b24aa15fed847b4f4
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 10:16:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir -p /data/outputs
 if [ -f /data/images.tar.gz -a -f /data/annotations.tar.gz ]; then
     tar -xf /data/images.tar.gz
     tar -xf /data/annotations.tar.gz
@@ -10,9 +9,9 @@ if [ -f /data/images.tar.gz -a -f /data/annotations.tar.gz ]; then
 fi
 
 if [ -f /data/oxford_segmentation.h5 -a -f /data/image.png ]; then
-    cp /data/oxford_segmentation.h5 /data/outputs/oxford_segmentation.h5
     echo "run execution"
     python main.py 0
     echo "ended execution"
+    cp /data/oxford_segmentation.h5 /data/outputs/oxford_segmentation.h5
 fi
 
