30346d3c830c05eedc44f6d2631d13b9bd2f0231
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 11:07:11 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Model start"
+pwd
+ls -l
+ls -l data/
+
 if [ -f data/images.tar.gz -a -f data/annotations.tar.gz ]; then
     tar -xf data/images.tar.gz
     tar -xf data/annotations.tar.gz
@@ -8,7 +12,7 @@ if [ -f data/images.tar.gz -a -f data/annotations.tar.gz ]; then
     echo "ended training"
 fi
 
-if [ -f data/oxford_segmentation.h5 -a -f data/image.png ]; then
+if [ -f data/oxford_segmentation.h5 -a -f data/images/image.jpg ]; then
     echo "run execution"
     python main.py 0
     echo "ended execution"
