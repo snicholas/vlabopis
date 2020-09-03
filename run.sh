ab31efad1cd5a562143b4e32792bc62633a9d097
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2020-09-03 14:08:58 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,10 +5,13 @@ ls -l
 
 
 if [ -f data/images.tar.gz -a -f data/annotations.tar.gz ]; then
+    mkdir -p /tmp/data
+    cp data/images.tar.gz /tmp/data/images.tar.gz
+    cp data/annotations.tar.gz /tmp/data/annotations.tar.gz
     echo "Unpackaging images.tar.gz"
-    tar -xf data/images.tar.gz
+    tar -xf /tmp/data/images.tar.gz
     echo "Unpackaging annotations.tar.gz"
-    tar -xf data/annotations.tar.gz
+    tar -xf /tmp/data/annotations.tar.gz
     ls -l data/    
     echo "run training"
     python main.py 1
